c20b5f6589cd640c63f69d45616ca8293fc38424
tarif66 <obrechon@gmail.com>
2025-05-20 16:37:37 +0200

    set up tuto eco

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,50 @@
-<script setup lang="ts">
-import { Button } from '@/components/ui/button'
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link> -
+    <router-link to="/basket">Shopping Bag (0)</router-link> 
+  </div>
+  <router-view/>
+</template>
+
+<script>
+
+  export default {
+
+  }
+  
 </script>
 
-<template>
-  <div>
-    <Button>Click me</Button>
-  </div>
-</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  max-width: 1280px;
+  margin: 80px auto;
+}
+
+#nav {
+  padding: 16px 0;
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  width: 100%;
+  text-align: center;
+  background-color: rgb(37, 37, 37);
+  color: white;
+
+
+  a {
+    color: white;
+    text-decoration: none;
+
+    &.router-link-exact-active {
+      color: #007bff;
+    }
+  }
+}
+</style>
